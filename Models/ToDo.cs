da2ebf423568fe5d74b2678bd21e8510ad66c699
k1.shahinpour <k1.shahinpour@gmail.com>
2020-04-25 16:13:20 +0400

    ToDo Model & List Page

---
+++ Models/ToDo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoPWA.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Complete { get; set; }
+        public DateTime DueDate { get; set; }
+        public String Note { get; set; }
+    }
+}
---
+++ Pages/Todo/ToDoList.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDoPWA.Data;
+using ToDoPWA.Models;
+
+namespace ToDoPWA.Pages.Todo
+{
+    public class ToDoListModel : PageModel
+    {
+        private readonly ToDoPWA.Data.ToDoPWAContext _context;
+
+        public ToDoListModel(ToDoPWA.Data.ToDoPWAContext context)
+        {
+            _context = context;
+        }
+
+        public IList<ToDo> ToDo { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            ToDo = await _context.ToDo.ToListAsync();
+        }
+    }
+}
